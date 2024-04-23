b377e34574500fd31f8436e1c7fcf48ea419ab5b
SiddardhaVibhoTech <siddardha.devarayapalli@vibhotech.com>
2024-04-23 16:52:29 +0530

    commented test code

--- app/app/test.py
+++ app/app/test.py
@@ -1,19 +1,19 @@
-'''
-sample test
-'''
+# '''
+# sample test
+# '''
 
-from django.test import SimpleTestCase
+# from django.test import SimpleTestCase
 
-from app import calc
+# from app import calc
 
-class CalcTest(SimpleTestCase):
-    def  test_addition(self):
-        a = 2
-        b = 5
-        res = calc.add(a,b)
-        self.assertEqual(res,7)
-    
-    def test_substact(self):
-        res = calc.substract(10,15)
-        self.assertEqual(res,5)
+# class CalcTest(SimpleTestCase):
+#     def  test_addition(self):
+#         a = 2
+#         b = 5
+#         res = calc.add(a,b)
+#         self.assertEqual(res,7)
+
+#     def test_substact(self):
+#         res = calc.substract(10,15)
+#         self.assertEqual(res,5)
         
